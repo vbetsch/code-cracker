91fe40ecf63f1884180d5d98b0b85d198f4305e5
vbetsch <vic.betsch@gmail.com>
2026-04-09 20:25:14 +0200

    feat: add decrypt logic

--- src/cypher.ts
+++ src/cypher.ts
@@ -1,5 +1,68 @@
 export class Cypher {
+  private _alphabet: string[] = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ];
+  private _decryptionKey: string[] = [
+    '!',
+    ')',
+    '"',
+    '(',
+    '£',
+    '*',
+    '%',
+    '&',
+    '>',
+    '<',
+    '@',
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+  ];
+
   public decrypt(message: string): string {
-    return 'hello';
+    let result: string = '';
+    for (let i = 0; i < message.length; i++) {
+      const char: string = message.charAt(i);
+      const index: number = this._decryptionKey.indexOf(char);
+      result += this._alphabet[index];
+    }
+    return result;
   }
 }
